779113f0bf031193b601cb0673951c52662a59d8
Tanmay <tanmay.milky33@gmail.com>
2021-01-23 18:17:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname+'/dist/bank-frontend'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/bank-frontend/index.html'));});
+app.use(express.static('./dist/bank-frontend'));
+// app.get('/*', function(req,res) {
+//   res.sendFile(path.join(__dirname+
+//     '/dist/bank-frontend/index.html'));});
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/bank-frontend/'}
+  );
+  });
 app.listen(process.env.PORT || 8080);
